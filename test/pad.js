569bb3af730c10fd37ef9b91682e62251604e4ce
Alex Robertson <alex@someoddpilot.com>
2015-04-01 09:00:33 -0500

    Testing pad

---
+++ test/pad.js
@@ -0,0 +1,10 @@
+const pad = require('../index').helpers.pad;
+const assert = require('assert');
+
+describe('pad', function () {
+  it('should pad text', function () {
+    assert.equal(pad('abc', 8), '  abc   ');
+    assert.equal(pad('abc', 8, '_-'), '_-abc_-_');
+    assert.equal(pad('abc', 3), 'abc');
+  });
+});
