eb5e3a2563418d7f4d218dca997317d42c36fff3
Alex Robertson <alex@someoddpilot.com>
2015-04-10 15:15:54 -0500

    Porting over less naive implementation

--- test/indefiniteArticle.js
+++ test/indefiniteArticle.js
@@ -3,7 +3,11 @@ const assert = require('assert');
 
 describe('indefiniteArticle', function () {
   it('should return the indefinite article for a string', function () {
-    assert.equal(indefiniteArticle('Foo Bar'), 'a');
-    assert.equal(indefiniteArticle('Apple'), 'an');
+    assert.equal(indefiniteArticle('Foo Bar'), 'a', 'a Foo Bar');
+    assert.equal(indefiniteArticle('Apple'), 'an', 'an Apple');
+    assert.equal(indefiniteArticle('once'), 'a', 'a uni');
+    assert.equal(indefiniteArticle('UKA'), 'a', 'a UKA');
+    assert.equal(indefiniteArticle('ybl'), 'an', 'an yblo');
+    assert.equal(indefiniteArticle('Yttrium'), 'an', 'an Yttrium');
   });
 });
